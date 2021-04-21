08b293e7f5a664ef809baf54bfb9f1ecaf7ee0a0
zou-hong-run <zhr19853149156@163.com>
2021-04-21 14:31:04 +0800

    修改路由文件

--- 6脚手架/demo/src/views/About.vue
+++ 6脚手架/demo/src/views/About.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <test></test>
   </div>
 </template>
+<script>
+import test from '@/components/test'
+export default {
+  components:{
+    test
+  }
+}
+</script>
