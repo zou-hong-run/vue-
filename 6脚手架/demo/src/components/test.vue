468c31ff0a619e096a96d8108e7985ad814dc05a
zou-hong-run <zhr19853149156@163.com>
2021-04-21 14:23:48 +0800

    vue脚手架安装路由

---
+++ 6脚手架/demo/src/components/test.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>{{data}}</h1>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {
+      data:'123'
+    }
+  }
+}
+</script>
+<style scoped>
+h1{
+ color: yellow;
+}
+</style>
