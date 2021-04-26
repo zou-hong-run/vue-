d27cecaa95b801d3a108edcb795778ba38743b5a
zou-hong-run <zhr19853149156@163.com>
2021-04-26 19:59:04 +0800

    手写kvue完成

--- 7手写vue/compile.js
+++ 7手写vue/compile.js
@@ -1,4 +1,5 @@
 //编译模板中vue模板特殊语法，初始化视图，更新视图
+
 //递归遍历dom树
 //判断节点类型，如果是文本，则判断是否是插值绑定
 //如果是元素，则遍历其”属性“判断是否是指令或事件，然后递归子元素
@@ -13,18 +14,21 @@ class Compile{
     }
   } 
   compile(el){
-    //遍历el树
+    //遍历el树 拿到el里面的所有孩纸
     const childNodes = el.childNodes
+
     Array.from(childNodes).forEach(node=>{
       //判断是否是元素
       if(this.isElement(node)){
         // console.log('编译元素'+node.nodeName)
         this.compileElement(node)
       }else if(this.isInter(node)){
-        // console.log('编译插值绑定'+node.textContent)
+        // {{console.log('编译插值绑定'+node.textContent)}}
         this.compileText(node)
       }else{  
-        console.log('编译其他元素'+node)
+        console.log('编译其他元素start=======================')
+        console.log(node)
+        console.log('编译其他元素end=========================')
       }
       //递归子节点
       if(node.childNodes&&node.childNodes.length>0){
@@ -32,20 +36,26 @@ class Compile{
       }
     })
   }
+  //判断是否是元素
   isElement(node){
     return node.nodeType === 1
   }
+  //判断元素或属性中的文本是否包含{{}}
   isInter(node){
+    //代表nodeType=3代表元素或属性中的文本内容
     //首先是文本标签，其次内容是{{xxx}}
     return node.nodeType ===3 && /\{\{(.*)\}\}/.test(node.textContent)
   }
+  //编译插值语法里面的变量
   compileText(node){
-    node.textContent = this.$vm[RegExp.$1]
+    // this.$vm[RegExp.$1]之所以可以获取是因为上有代理将数据绑定到Kvue身上了
+    // node.textContent = this.$vm[RegExp.$1]
+    this.update(node,RegExp.$1,'text')
   }
   //编译节点
+  //当前节点是元素
   compileElement(node){
-    //当前节点是元素
-    //遍历器属性列表
+    //      遍历获取元素的所有属性
     const nodeAttrs = node.attributes
     Array.from(nodeAttrs).forEach(attr=>{
       //规定：指令以k-xx="00"定义
@@ -58,15 +68,36 @@ class Compile{
       }
     })
   }
+  //判断是否是指令，去除其他的东西 比如value='123'
   isDirective(attr){
     return attr.indexOf('k-') === 0
   }
   // k-text
   text(node,exp){
-    node.textContent = this.$vm[exp]
+    // node.textContent = this.$vm[exp]
+    this.update(node,exp,'text')
   } 
   //k-html
   html(node,exp){
-    node.innerHTML = this.$vm[exp]
+    // node.innerHTML = this.$vm[exp]
+    this.update(node,exp,'html')
   }
+  //指令对应函数
+  textUpdater(node,value){
+    node.textContent = value
+  }
+  htmlUpdater(node,value){
+    node.innerHTML = value
+  }
+  //统一改变函数
+  update(node,exp,dir){
+    //初始化
+    //指令对应的更新函数xxUpdater
+    const fn = this[dir+'Updater']
+    fn && fn(node,this.$vm[exp])
+    //更新 封装一个更新函数，可以更新对应dom
+    new Watcher(this.$vm,exp,function(val){
+      fn && fn(node,val)
+    })
+  } 
 }
--- 7手写vue/kvue.js
+++ 7手写vue/kvue.js
@@ -6,26 +6,36 @@
  * Watcher:执行更新函数（更新dom）
  * Dep:管理多个Watcher,批量更新
  */
- function defineReactive(obj,key,val){
-  //做一个递归
+
+//将单个数据变为响应式
+function defineReactive(obj,key,val){
+  //做一个递归（如果传递过来的val是一个对象，那么还需要进行遍历，
+  //把val对象里面的值也变成响应式的
   observe(val)
+  //创建一个Dep和当前key一一对应
+  const dep = new Dep()
   //对传入obj进行拦截访问
   Object.defineProperty(obj,key,{
     get(){
-      console.log('get:'+key)
+      console.log('获取 :'+key)
+      //依赖收集发生地
+      Dep.target && dep.addDep(Dep.target)
       return val
     },
     set(newVal){
       if(newVal !== val){
-        console.log('set:'+key+'='+newVal)
-        // 如果传递过来的新值，是一个对象那么也要将它作为响应式数据
+        console.log('设置:'+key+'='+newVal)
+        // 如果设置的值是一个对象，那么也要将调用该函数将他变为响应式数据
         observe(newVal)
         val = newVal
+        //执行更新函数
+        // watcher.forEach((w)=>{w.update()})
+        dep.notify()
       }
     }
   })
 }
-//把对象中所有属性都设置为响应式
+//遍历对象中的所有属性，然后都转变为响应式数据
 function observe(obj){
   if(typeof obj !== 'object' || obj == null){
     //希望传入的是对象
@@ -34,6 +44,7 @@ function observe(obj){
   //创建Observer实例
   new Observer(obj)
 }
+//在已经定义好的对象基础上，追加响应式数据
 function set(obj,key,val){
   defineReactive(obj,key,val)
 }
@@ -41,14 +52,14 @@ function set(obj,key,val){
 //代理函数，方便用户直接访问$data中的数据
 function porxy(vm,sourceKey){
   Object.keys(vm[sourceKey]).forEach(key=>{
-    //往kvue对象身上添加定义一个新属性，可以修改
+    //往kvue对象身上添加一个新属性，可以修改
     Object.defineProperty(vm,key,{
       get(){
         return vm[sourceKey][key]
       },
       set(newVal){
         //将kvue.$data对象身上的值覆盖，重新做数据响应化
-        vm.sourceKey[key] = newVal
+        vm[sourceKey][key] = newVal
       }
     })
   })
@@ -60,16 +71,16 @@ class KVue{
     //保存选项
     this.$options = options
     this.$data = options.data
-    //响应化处理
-    // console.log(this)
+    //将data里面的数据做响应化处理
     observe(this.$data)
-    // //做一个代理（将$data值放到kvue身上）
+    //做一个代理（将$data值放到kvue身上）
     porxy(this,"$data")
-    //创建编译器
+    //创建编译器 解析编译{{}} k-html..........
     new Compile(options.el,this)
   }
 }
-//根据对象类型觉得如何做响应化
+
+//根据对象类型，决定如何做何种类型的 响应化
 class Observer{
   constructor(value){
     this.value = value
@@ -89,15 +100,35 @@ class Observer{
   //数组数据响应化，。。。
 
 }
-//观察者(保存更新函数)
-let watcher = []
+
+//创建观察者(保存更新函数)，值发生变化调用更新函数
+// let watcher = []
 class Watcher{
   constructor(vm,key,updateFn){
     this.vm = vm 
     this.key = key
-    this.update = updateFn
+    this.updateFn = updateFn
+    // watcher.push(this)
+    //Dep.target静态属性上设置为当前watcher实例
+    Dep.target = this
+    this.vm[this.key] //读取触发了getter
+    Dep.target = null //收集完就置空 
   }
   update(){
+    //改变this
     this.updateFn.call(this.vm,this.vm[this.key])
+  }
+}
+
+//Dep:依赖，管理某个key相关所有Watcher实例
+class Dep{
+  constructor(){
+    this.deps = []
+  }
+  addDep(dep){
+    this.deps.push(dep)
+  }
+  notify(){
+    this.deps.forEach(dep=>dep.update())
   }
 }
